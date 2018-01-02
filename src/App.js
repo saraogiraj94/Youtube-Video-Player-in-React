0f0713d9846ea63bd6ed363fe2c47e5c5da8e58b
RAJSARAOGI\Raj Saraogi <saraogi.raj40@gmail.com>
2018-01-02 19:42:43 +0530

    Added conditional content

--- src/App.js
+++ src/App.js
@@ -10,56 +10,73 @@ class App extends Component {
   //state is reserved word
   //We can access by state property
 
-  state={
-    persons:[
-      {name:'Raj',age:23},
-      {name:'Sharma',age:21},
-      {name:'Manu',age:25}
+  state = {
+    persons: [
+      { name: 'Raj', age: 23 },
+      { name: 'Sharma', age: 21 },
+      { name: 'Manu', age: 25 }
     ],
-    other:"Other details"
+    showPersons: false,
+    other: "Other details"
   }
 
   //We use terminlogy handler to know that is called when an event occours
   //we have created function as property as we need to use this do relate to the class
-  
-  switchNameHandler = ()=>{
+
+  switchNameHandler = () => {
     //Here we will be edit the state
     //We should not mutuate state like this it gives warning 
     //this.state.persons[1].name="Saraogi";
-    
+
     //We use setState Method in Components class
     //This will update the property without changing other properties
-    
+
     this.setState({
-      persons:[
-        {name:'RajKumar',age:23},
-        {name:'Saraogi',age:21},
-        {name:'Manu',age:25}
-      ] 
-    }) ;  
+      persons: [
+        { name: 'RajKumar', age: 23 },
+        { name: 'Saraogi', age: 21 },
+        { name: 'Manu', age: 25 }
+      ]
+    });
   }
 
   //This is 2 way binding type example
-  changeNameHandler=(event)=>{
+  changeNameHandler = (event) => {
     this.setState({
-      persons:[
-        {name:event.target.value,age:23},
-        {name:'Saraogi',age:21},
-        {name:'Manusasd',age:25}
-      ] 
-    }) ;
+      persons: [
+        { name: event.target.value, age: 23 },
+        { name: 'Saraogi', age: 21 },
+        { name: 'Manusasd', age: 25 }
+      ]
+    });
+  }
+
+  togglePersonHandler = () => {
+    const doesShow=this.state.showPersons;
+    this.setState({showPersons:!doesShow});
   }
 
   render() {
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+        <Person name={this.state.persons[0].name} age="23" changed={this.changeNameHandler} />
+        <Person name={this.state.persons[1].name} age="21" />
+        <Person name={this.state.persons[2].name} age="24">Playing Football</Person>
+      </div>);
+    }
+
+
     return (
       <div className="App">
         <h1> Hi I am React </h1>
-       
+
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age="23" changed={this.changeNameHandler}  />
-        <Person name={this.state.persons[1].name} age="21" />
-        <Person name={this.state.persons[1].name} age="21" />
-        <Person name={this.state.persons[2].name} age="24">Playing Football</Person>
+        <button onClick={this.togglePersonHandler}>Toggle Person</button>
+        {persons}
+
       </div>
     );
   }
